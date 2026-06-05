5af601484c40cc39980f1cf598e332e3547dd6e1
Zorcis Calderon <zorciscalderon793@gmail.com>
2026-06-05 16:51:25 -0400

    Air Draw Proyect

--- src/webcam.py
+++ src/webcam.py
@@ -1,18 +1,19 @@
 import cv2
 
-
 def webcam_frames(camera_index = 0):
-    cap = cv2.VideoCapture(camera_index)
+
+    cap = cv2.VideoCapture(camera_index) 
     
     if not cap.isOpened():
         raise RuntimeError("Error: Could not open webcam.")
         
-    
     while True:
-        success, frame = cap.read()
+        success, frame = cap.read() 
         if not success:
             break
+
         frame = cv2.flip(frame, 1)
+
         yield frame
 
     cap.release()
